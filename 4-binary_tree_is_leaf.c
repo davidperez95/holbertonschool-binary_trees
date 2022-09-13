7a0e57fe143697225522cbe6d57b9c036f954792
David Perez <daperezcruz@gmail.com>
2022-09-13 18:03:01 -0500

    code fix on task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->left || node->right || node == NULL)
-		return (0);
-	return (1);
+	if (node && !node->left && !node->right)
+		return (1);
+	return (0);
 }
