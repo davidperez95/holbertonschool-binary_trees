e16c50c6528cdc9bc77f6da09c5971cc88afeca1
David Perez <daperezcruz@gmail.com>
2022-09-13 19:18:05 -0500

    code fix on task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,19 +7,11 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t size = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		size++;
-
-	if (tree->left != NULL)
-		size++;
-
-	if (tree->right != NULL)
-		size++;
-
-	return (size);
+	if (tree && tree->left == NULL && tree->right == NULL)
+		return (1);
+	else
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
