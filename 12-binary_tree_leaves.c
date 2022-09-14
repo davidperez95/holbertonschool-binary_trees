f18ed2dc19a3a49bc2b37a5a773fcf72942ad338
David Perez <daperezcruz@gmail.com>
2022-09-13 19:03:57 -0500

    betty fix on task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,23 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * @brief 
- * 
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: the number of leaves
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t size = 0;
+	size_t size = 0;
 
-    if (tree == NULL)
-        return (0);
-    
-    if (tree->left == NULL && tree->right == NULL)
-        size++;
-    
-    if (tree->left != NULL)
-        size++;
-    
-    if (tree->right != NULL)
-        size++;
-    return (size);
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		size++;
+
+	if (tree->left != NULL)
+		size++;
+
+	if (tree->right != NULL)
+		size++;
+
+	return (size);
 }
