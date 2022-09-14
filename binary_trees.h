1c89887b12f191d2f92fe599e699a13e7fa5f303
David Perez <daperezcruz@gmail.com>
2022-09-14 17:48:21 -0500

    task 100

--- binary_trees.h
+++ binary_trees.h
@@ -1,14 +1,14 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-/* Librarys */ 
+/* Librarys */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
 
-/* Structure */ 
+/* Structure */
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -20,10 +20,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -53,5 +53,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second);
 
 #endif
